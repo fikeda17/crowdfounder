fbf611dd2aeb46e26d41c8e5a9e431a635c8dccd
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-07-21 14:46:09 -0400

    Generating the projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,46 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def create
+    @project = Project.new(product_params)
+
+    if @project.save
+      redirect_to project_url
+    else
+      render :new
+    end
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      redirect_to project_path(@project)
+    else
+      render :edit
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :description, :funding_goal, :start_date, :end_date, :photo)
+  end
+end
