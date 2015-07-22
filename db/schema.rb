3bbafbcbfe76c9df1a200c64bff1eb831cab5bb5
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-07-22 15:25:12 -0400

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721203526) do
+ActiveRecord::Schema.define(version: 20150722185322) do
+
+  create_table "pledges", force: :cascade do |t|
+    t.integer  "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +28,14 @@ ActiveRecord::Schema.define(version: 20150721203526) do
     t.string   "photo"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "rewards", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "amount"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
